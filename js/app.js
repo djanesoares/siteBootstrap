a20e195fd3a416444b4705fcb86bd335c5462bda
Djane Soares <djanesoares@gmail.com>
2024-10-20 10:12:01 +0100

    corrigido as transiçoes dos slides do carousel

---
+++ js/app.js
@@ -0,0 +1,5 @@
+$(".carousel").carousel({
+  interval: 3000, // Tempo entre as transições (em milissegundos)
+  pause: "hover", // Pausa a transição quando o mouse está sobre o slide
+  ride: "carousel", // Inicia automaticamente
+});
